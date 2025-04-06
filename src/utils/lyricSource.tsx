83b3b48ba56801d9e3745c0bc4f2553ca18fe243
HermanKassler <tore.gaani@gmail.com>
2025-04-06 11:32:17 +0200

    add basic api calls to lyrics.ovh

---
+++ src/utils/lyricSource.tsx
@@ -0,0 +1,20 @@
+import { PROXY_URL } from "./lyricApiConfig.js";
+
+function getResponseACB(response) {
+  if (!response.ok) throw new Error("HTTP status code: " + response.status);
+  return response.json();
+}
+
+// Reference : https://lyricsovh.docs.apiary.io/#reference/0/lyrics-of-a-song/search?console=1
+export function getLyrics(songParams) {
+  var url: string = PROXY_URL +
+    "/" + songParams.artist +
+    "/" + songParams.song
+  // new URLSearchParams(pageParams)
+  return fetch(
+    url,
+  )
+    .then(getResponseACB)
+}
+
+

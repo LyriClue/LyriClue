0b4ee3b72105c390dd8ff259d6514a572c343d8a
William Krull <krull@kth.se>
2025-04-08 12:55:06 +0200

    changes to gameView

--- src/views/gameView.tsx
+++ src/views/gameView.tsx
@@ -1,7 +1,32 @@
 function GameView(props: any) {
     console.log(props.lyric);
     return (
-        <div>
+        <div className="h-screen w-screen ">
+        {/* Background image */}
+        <img
+          className="fixed top-0 left-0 w-full h-full object-cover"
+          src="src/assets/background.jpg"
+          alt="Background"
+        />
+        <div className="w-full h-full left-0 top-0 fixed bg-zinc-300/20" />
+  
+        {/* Logo */}
+        <div className="absolute top-4 left-0 w-full">
+          <h1 className="text-2xl text-black mainfont text-center md:text-left [text-shadow:_0px_4px_8px_rgb(255_255_255_/_1.00)]">
+            LyriClue
+          </h1>
+        </div>
+  
+        {/* Main content */}
+        <div className="relative items-center">
+            <GameComponent />
+        </div>
+      </div>
+    )
+
+    function GameComponent(){
+        return(
+            <div>
             <span>
                 <b>
                     {"... " + props.lyric + " ..."}
@@ -17,7 +42,8 @@ function GameView(props: any) {
                 <input type="submit" value="submit" />
             </form>
         </div>
-    )
+        );
+    }
 }
 
 export { GameView }

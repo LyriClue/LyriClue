2d088d5cd21b15a23614b585e86c8b2ad44b7b8b
FMurkz <feliciamurkes@gmail.com>
2025-04-24 15:15:00 +0200

    Made the button work using a users already existing playlist #63

--- src/presenters/landingPresenter.tsx
+++ src/presenters/landingPresenter.tsx
@@ -6,9 +6,19 @@ import { LandingView } from "../views/landingView";
 export function LandingPresenter(props: any) {
   console.log("landingview");
   return (
-    <LandingView playOwnPlaylist={PlayOwnPlaylistsACB} />
+    <LandingView playOwnPlaylist={PlayOwnPlaylistsACB} 
+                playDailyPlaylist={playDailyPlaylistACB}
+    />
   );
+  function playDailyPlaylistACB() {
+    console.log("play own playlists");
+    if (!props.model.playlists) {
+      props.model.retrievePlaylists()
+    }
 
+    props.model.setCurrentPlaylist(props.model.playlists.items[9])
+    props.model.startGame()
+  }
   function PlayOwnPlaylistsACB() {
     if (!props.model.playlists) {
       props.model.retrievePlaylists()

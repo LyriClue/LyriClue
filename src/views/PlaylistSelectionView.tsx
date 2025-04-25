b64c09cdeef2c1a775b1e6efc2ad5a27731e21de
FMurkz <feliciamurkes@gmail.com>
2025-04-25 17:23:42 +0200

    changed to a smaller image #71

--- src/views/PlaylistSelectionView.tsx
+++ src/views/PlaylistSelectionView.tsx
@@ -3,7 +3,7 @@ export function PlaylistSelectionView(props: any) {
     <div className="relative w-full h-[30%]">
       <img
         className="fixed top-0 left-0 w-full h-full object-cover z-0"
-        src="https://lagn9w7j0a.ufs.sh/f/P2ljk8lEtN0jrczxxbYEFvcduPT5sY8H9An067qykmOl4RNo"
+        src="https://lagn9w7j0a.ufs.sh/f/P2ljk8lEtN0jNStY88rhRy7bLpuOx8lWi1cdXHE6DMwT9Pvj"
         alt="Background"
       />
       <div className="fixed top-0 left-0 w-full h-full bg-zinc-300/20 z-10" />

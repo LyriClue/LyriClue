1e7a46bcc9d64b544cf2c76d1c3a58b95b7648dd
HermanKassler <tore.gaani@gmail.com>
2025-04-29 15:19:26 +0200

    consolidate guess/correct into one component

--- src/views/postGuessView.tsx
+++ src/views/postGuessView.tsx
@@ -9,8 +9,8 @@ export function PostGuessView(props: any) {
       {/* Main content */}
       <div className="top-1/10 relative z-10 flex flex-col justify-between pl-4 items-center ">
 
-        {yourGuess()}
-        {correctGuess()}
+        {songInformationComponent("Your Guess:", props.songGuess, props.artistGuess)}
+        {songInformationComponent("Correct Answer", props.correctSong.title, props.correctSong.artist)}
         <button onClick={props.nextRound} className="font-mono h-20 w-35 rounded-full bg-white hover:bg-gray-100 transition-colors shadow-md">
           Next Song
         </button>
@@ -19,19 +19,19 @@ export function PostGuessView(props: any) {
 
   )
 
-  function yourGuess() {
+  function songInformationComponent(header: string, title: string, artist: string) {
     return (
       <div className="w-full md:w-2/3 flex flex-col items-center p-10">
         <h2 className="text-6xl text-black mainfont text-center md:text-left [text-shadow:_0px_4px_8px_rgb(255_255_255_/_1.00)]">
-          Your Guess:
+          {header}
         </h2>
         <br />
         <div className="relative bg-white p-4  shadow-[-8px_8px_0px_0px_rgba(0,0,0,1)] w-[500px]">
           <p className="text-center text-black text-2xl font-mono leading-10 mb-1">
-            {"Song title: " + props.songGuess}
+            {"Song title: " + title}
           </p>
           <p className="text-center text-black text-2xl font-mono leading-10">
-            {"Artist: " + props.artistGuess}
+            {"Artist: " + artist}
           </p>
         </div>
       </div>
@@ -39,24 +39,6 @@ export function PostGuessView(props: any) {
 
   }
 
-  function correctGuess() {
-    return (
-      <div className="w-full md:w-2/3 flex flex-col items-center p-10">
-        <h2 className="text-6xl text-black mainfont text-center md:text-left [text-shadow:_0px_4px_8px_rgb(255_255_255_/_1.00)]">
-          Correct answer:
-        </h2>
-        <br />
-        <div className="relative bg-white p-4  shadow-[-8px_8px_0px_0px_rgba(0,0,0,1)] w-[500px]">
-          <p className="text-center text-black text-2xl font-mono leading-10 mb-1">
-            {"Song title: " + props.correctSong.title}
-          </p>
-          <p className="text-center text-black text-2xl font-mono leading-10">
-            {"Artist: " + props.correctSong.artist}
-          </p>
 
-        </div>
-      </div>
-    )
-  }
 
 }

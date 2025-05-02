82f1e2b1a48fec1f11028415d22da5697aeb2286
HermanKassler <99051506+HermanKassler@users.noreply.github.com>
2025-05-02 10:27:33 +0200

    send user to login page if spotify login fails (#58)

--- src/presenters/SpotifyLanding.tsx
+++ src/presenters/SpotifyLanding.tsx
@@ -1,15 +1,23 @@
 import { SuspenseView } from "../views/suspenseView"
 import { getTokenFromUrl } from "./AuthPresenter"
+import { useEffect } from "react";
+import { urlContains } from "../utils/pathUtil";
 import { signIn } from "../utils/firestoreModel";
 
-export function SpotifyLanding(props: { model: { token: string, setToken(newToken: string): void }; }) {
-  props.model.setToken(getTokenFromUrl().access_token)
-  signIn(props.model.token).then(navigateToLanding)
-
+export function SpotifyLanding(props: { model: { setToken(newToken: string): void }; }) {
+  if (urlContains("error")) {
+    window.history.pushState("", "", "/");
+    dispatchEvent(new PopStateEvent('popstate', {}))
+  } else {
+    props.model.setToken(getTokenFromUrl().access_token)
+    signIn(props.model.token).then(navigateToLanding)
+  }
+  
   return (
     <SuspenseView promise={Promise.resolve("logging in")} />
   )
 }
+
 function navigateToLanding() {
   window.history.pushState("", "", "/landing");
   dispatchEvent(new PopStateEvent('popstate', {}))

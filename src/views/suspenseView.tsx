acb00a51e1bf91df5a33a698d4412ccfc5fcfcc1
HermanKassler <tore.gaani@gmail.com>
2025-05-02 13:04:48 +0200

    center suspense image

--- src/views/suspenseView.tsx
+++ src/views/suspenseView.tsx
@@ -9,5 +9,9 @@ export function SuspenseView(props: SuspenseViewProps) {
   if (!props.promise) return <span>{props.noPromiseMessage || "no data"}</span>;
   if (props.error) return <span>{props.error.toString()}</span>;
 
-  return (< img src="https://brfenergi.se/iprog/loading.gif" alt="Loading..." />)
+  return (
+    <div className="flex w-full h-full justify-center">
+      < img src="https://brfenergi.se/iprog/loading.gif" alt="Loading..." />
+    </div>
+  )
 }

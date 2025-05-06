01bc8fe9da478866b5d456e67539b30c7b5d75c4
HermanKassler <99051506+HermanKassler@users.noreply.github.com>
2025-05-06 15:37:40 +0200

    extract text styling to a constant (#114)

--- src/views/ViewUtils.tsx
+++ src/views/ViewUtils.tsx
@@ -1,5 +1,7 @@
-export function Background() {
+export const blackText = " text-black mainfont [text-shadow:_0px_4px_8px_rgb(255_255_255_/_1.00)] "
+export const whiteText = " text-white mainfont [text-shadow:_0px_4px_8px_rgb(255_255_255_/_1.00)] "
 
+export function Background() {
   return (
     <div>
       <img
@@ -15,9 +17,9 @@ export function Background() {
 
 export function Logo(position: string = "relative") {
   return (<div className={position + " top-4 left-0 "}>
-    <h1 className="text-2xl md:ml-5 text-black mainfont text-center md:text-left [text-shadow:_0px_4px_8px_rgb(255_255_255_/_1.00)]">
+    <h1 className={"text-2xl md:ml-5  text-center md:text-left " + blackText}>
       LyriClue
     </h1>
-  </div>
+  </div >
   )
 }

f70ce38e760dce684fcf74e0b8ef1eae891e7fec
HermanKassler <tore.gaani@gmail.com>
2025-05-06 15:16:02 +0200

    extract text styling to a constant

--- src/views/PlaylistSelectionView.tsx
+++ src/views/PlaylistSelectionView.tsx
@@ -1,4 +1,4 @@
-import { Background } from "./ViewUtils";
+import { Background, blackText } from "./ViewUtils";
 
 export function PlaylistSelectionView(props: any) {
   return (
@@ -7,7 +7,7 @@ export function PlaylistSelectionView(props: any) {
 
       {/* Playlist Content */}
       <div className="relative z-20 p-8">
-        <h1 className="text-3xl mainfont text-black mb-4 [text-shadow:_0px_4px_8px_rgb(255_255_255_/_1.00)]">Choose a Playlist!</h1>
+        <h1 className={blackText + "text-3xl  mb-4"}>Choose a Playlist!</h1>
         <table className="w-full mb-4 bg-black/50">
           <tbody>
             {props.playlists.map(renderPlaylistsCB)}

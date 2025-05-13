f0bd05fa6f57ccff4353c2171eb01d0333124f0a
SkrodS <robin.widjeback@gmail.com>
2025-05-13 17:09:22 +0200

    fix text size

--- src/views/CountdownView.tsx
+++ src/views/CountdownView.tsx
@@ -3,7 +3,7 @@ import { blackText } from "./ViewUtils";
 export function CountdownView(props: any) {
   return (
     <div className="flex flex-col relative w-screen items-center">
-      <h2 className={blackText}>Game starts in...</h2>
+      <h2 className={"text-6xl" + blackText}>Game starts in...</h2>
       <div className="m-auto  align-middle">
         <h2 className={"text-6xl" + blackText}>
           {props.currentValue}

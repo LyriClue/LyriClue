b715ad8de50d7ebac3aa7395777ddd6dc8af966d
William Krull <krull@kth.se>
2025-05-13 13:09:40 +0200

    added new gif for suspenseView

--- src/views/suspenseView.tsx
+++ src/views/suspenseView.tsx
@@ -17,8 +17,8 @@ export function SuspenseView(props: SuspenseViewProps) {
   }
 
   return (
-    <div className="flex flex-cols items-center justify-center relative z-10 bg-black/40 rounded-xl">
-      < img className="drop-shadow-xl" src="https://brfenergi.se/iprog/loading.gif" alt="Loading..." />
+    <div className="flex flex-cols items-center justify-center relative z-10 bg-white/55 rounded-xl">
+      < img className="drop-shadow-xl h-50 w-auto" src="https://lagn9w7j0a.ufs.sh/f/P2ljk8lEtN0jG0qIs6Fct6mF7z0alWSEvNUdpHeqDbCwJr1G" alt="Loading..." />
     </div >
   )
 

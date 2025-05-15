434235b154213526835da23c0635c7d9f5c27079
HermanKassler <tore.gaani@gmail.com>
2025-05-15 14:53:46 +0200

    finish up basic functioning component

--- src/presenters/gamePresenter.tsx
+++ src/presenters/gamePresenter.tsx
@@ -2,8 +2,6 @@ import { observer } from "mobx-react-lite";
 import { GameView } from "../views/gameView.tsx";
 import { SuspenseView } from "../views/suspenseView.tsx";
 import { Model } from "../Model.tsx";
-import { useCallback } from "react";
-import { LrcLine } from "react-lrc";
 
 
 

a6a507cb7e5e3cd0fd4b7b5759f9704589d6068d
William Krull <krull@kth.se>
2025-05-19 15:21:46 +0200

    added short text describing game on login page

--- src/views/AuthView.tsx
+++ src/views/AuthView.tsx
@@ -7,18 +7,27 @@ export function AuthView(props: any) {
 
       {/* Logo */}
       <div className="absolute top-1/3 md:top-1/7 left-0 w-full text-3xl md:text-6xl ">
-        <h1 className="logofont text-center">
+        <h1 className="logofont text-center ">
           LyriClue
         </h1>
       </div>
+
       {/* Main content */}
-      <div className="relative z-10 flex flex-col md:flex-row items-center top-1/2 justify-center ">
-        <button onClick={onSpotifyLoginACB} className="m-2.5">
-          Login With Spotify
-        </button>
-        <button onClick={onGuestLoginACB} className="m-2.5">
-          Play as Guest
-        </button>
+      <div className="flex h-screen">
+        <div className="m-auto relative z-10 flex flex-col items-center justify-center">
+          <div className="w-[500px] opacity-75 bg-black mb-4 my-25 rounded-xl">
+              <div className="text-[20px] m-2">Guess songs from lyrics! Link Spotify to turn your playlists into quizzes, or play daily challenges as a guest. Prove you’re the ultimate lyric sleuth. 🎵🔍</div>
+          </div>
+    
+          <div className="flex flex-row">
+            <button onClick={onSpotifyLoginACB} className="m-2.5">
+              Login With Spotify
+            </button>
+            <button onClick={onGuestLoginACB} className="m-2.5">
+              Play as Guest
+            </button>
+          </div>
+        </div>
       </div>
 
     </div>

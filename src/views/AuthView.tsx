be3c74ed0988051ab955c1ec81d629f7a51515fa
William Krull <krull@kth.se>
2025-05-19 16:33:25 +0200

    logo no longer overlaps vertically with other elements for smaller screens

--- src/views/AuthView.tsx
+++ src/views/AuthView.tsx
@@ -5,17 +5,19 @@ export function AuthView(props: any) {
   return (
     <div className="h-screen w-screen ">
 
-      {/* Logo */}
-      <div className="absolute top-1/3 md:top-1/7 left-0 w-full text-3xl md:text-6xl ">
-        <h1 className="logofont text-center ">
-          LyriClue
-        </h1>
-      </div>
-
       {/* Main content */}
       <div className="flex h-screen">
         <div className="m-auto relative z-10 flex flex-col items-center justify-center">
-          <div className="w-[500px] opacity-75 bg-black mb-4 my-25 rounded-xl">
+
+          {/* Logo */}
+          <div className="left-0 w-full text-3xl md:text-6xl">
+            <h1 className="logofont text-center ">
+              LyriClue
+            </h1>
+          </div>
+
+          {/* Description */}
+          <div className="w-[500px] opacity-75 bg-black mb-4 my-5 rounded-xl">
               <div className="text-[20px] m-2">Guess songs from lyrics! Link Spotify to turn your playlists into quizzes, or play daily challenges as a guest. Prove you’re the ultimate lyric sleuth. 🎵🔍</div>
           </div>
     
